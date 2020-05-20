b452fb234bb23f65836e1446745e30dff1a41e9d
chjin <seedevk8s@gmail.com>
2020-05-20 16:25:29 +0900

    parameter add

--- src/HttpConnectionUtil.java
+++ src/HttpConnectionUtil.java
@@ -17,7 +17,7 @@ public class HttpConnectionUtil {
      * @param pList : 파라미터 객체 (HashMap<String,String>)
      *
      */
-    public static String postRequest(String pURL, HashMap < String, String > pList) {
+    public static String sendMessage(String pURL, HashMap < String, String > pList, String message, String encType) {
 
         String myResult = "";
 
@@ -25,14 +25,19 @@ public class HttpConnectionUtil {
             //   URL 설정하고 접속하기
             URL url = new URL(pURL); // URL 설정
 
-            HttpURLConnection http = (HttpURLConnection) url.openConnection(); // 접속
+            HttpURLConnection mConn = (HttpURLConnection) url.openConnection(); // 접속
             //--------------------------
             //   전송 모드 설정 - 기본적인 설정
             //--------------------------
-            http.setDefaultUseCaches(false);
-            http.setDoInput(true); // 서버에서 읽기 모드 지정
-            http.setDoOutput(true); // 서버로 쓰기 모드 지정
-            http.setRequestMethod("POST"); // 전송 방식은 POST
+            mConn.setRequestProperty("Content-type", "text/plain;charset=".concat(encType));
+            mConn.setRequestMethod("POST");
+            mConn.setConnectTimeout(10000);
+            mConn.setDoOutput(true);
+            //mConn.setFixedLengthStreamingMode(message.getBytes().length);
+            mConn.setDefaultUseCaches(false);
+            mConn.setDoInput(true); // 서버에서 읽기 모드 지정
+            mConn.setDoOutput(true); // 서버로 쓰기 모드 지정
+            mConn.setRequestMethod("POST"); // 전송 방식은 POST
 
 
 
@@ -40,7 +45,7 @@ public class HttpConnectionUtil {
             // 헤더 세팅
             //--------------------------
             // 서버에게 웹에서 <Form>으로 값이 넘어온 것과 같은 방식으로 처리하라는 걸 알려준다
-            http.setRequestProperty("content-type", "application/x-www-form-urlencoded");
+            mConn.setRequestProperty("content-type", "application/x-www-form-urlencoded");
 
 
             //--------------------------
@@ -60,7 +65,7 @@ public class HttpConnectionUtil {
                 }
             }
 
-            OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "UTF-8");
+            OutputStreamWriter outStream = new OutputStreamWriter(mConn.getOutputStream(), "EUC-KR");
             PrintWriter writer = new PrintWriter(outStream);
             writer.write(buffer.toString());
             writer.flush();
@@ -75,7 +80,7 @@ public class HttpConnectionUtil {
             //--------------------------
             //   서버에서 전송받기
             //--------------------------
-            InputStreamReader tmp = new InputStreamReader(http.getInputStream(), "UTF-8");
+            InputStreamReader tmp = new InputStreamReader(mConn.getInputStream(), "UTF-8");
             BufferedReader reader = new BufferedReader(tmp);
             StringBuilder builder = new StringBuilder();
             String str;
--- src/Test.java
+++ src/Test.java
@@ -3,11 +3,13 @@ import java.util.HashMap;
 public class Test {
 
     public static void main(String[] args) {
-        String url = "https://fb.com"; 	//URL
+        String url = "https://www.daum.net/"; 	//URL
         HashMap<String, String> param = new HashMap<String, String>();
         param.put("name", "이순신");	//PARAM
+        String encType = "EUC-KR";
+        String message = "040010020200519142141FICWON000L05000001LP000000000000000001001AAAAAAAAAABBBBBBBBBBCCCCCCCCCCDDDDDDDDDDEEEEEEEEEEFFFFFFFFFFGGGG 000";
 
-        String resp = HttpConnectionUtil.postRequest(url, param);
+        String resp = HttpConnectionUtil.sendMessage(url, param, message, encType);
 
         System.out.println(resp);
     }
